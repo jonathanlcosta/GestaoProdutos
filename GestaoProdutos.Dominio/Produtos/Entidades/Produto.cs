da6d8fc1377ef2c9dd7e84df07435f90184fcadf
Jonathan Costa <nick.costa12@hotmail.com>
2024-01-05 14:49:45 -0300

    GEN-01 - Testa nova funcionalidade.

--- GestaoProdutos.Dominio/Produtos/Entidades/Produto.cs
+++ GestaoProdutos.Dominio/Produtos/Entidades/Produto.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GestaoProdutos.Dominio.Execoes;
 using GestaoProdutos.Dominio.Fornecedores.Entidades;
 using GestaoProdutos.Dominio.Produtos.Enumeradores;
